43a9b512fce205dd1167b57f033ba7bbe093e4b2
Harry McKillen <1691128+harrymckillen@users.noreply.github.com>
2025-11-26 09:29:59 +0000

    Outrun theme: default unknown text if not metadata, offset text if no arturl

--- outrun/player.sh
+++ outrun/player.sh
@@ -17,10 +17,15 @@ shorten() {
 
 IFS='|' read -r status title artist album arturl <<< "$(playerctl metadata --format '{{status}}|{{title}}|{{artist}}|{{album}}|{{mpris:artUrl}}')"
 
-echo -e "\${offset 100}\${voffset 10}\${font1}STATUS: \${font}${status^^}\n"
-echo "\${offset 100}\${voffset -10}\${font1}Track: \${font}$(shorten "$title")"
-echo "\${offset 100}\${font1}Artist: \${font}$(shorten "$artist")"
-echo "\${offset 100}\${font1}Album: \${font}$(shorten "$album")"
+offset=10
+if [[ -n "$arturl" ]]; then
+  offset=100
+fi
+
+echo -e "\${offset $offset}\${voffset 10}\${font1}STATUS: \${font}${status^^}\n"
+echo "\${offset $offset}\${voffset -10}\${font1}Track: \${font}$(shorten ${title:-Unknown})"
+echo "\${offset $offset}\${font1}Artist: \${font}$(shorten "${artist:-Unknown}")"
+echo "\${offset $offset}\${font1}Album: \${font}$(shorten "${album:-Unknown}")"
 
 # Download cover only if URL is not empty and has changed
 if [[ -n "$arturl" ]]; then
